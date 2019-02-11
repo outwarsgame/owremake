6cf9f54faf9cacee38b0c4b12f9d9a67ac03b60d
Mortuis <xf_2020@hotmail.com>
2019-02-11 22:53:38 +0100

    massive file update following newer unity version. Starting GUI.

---
+++ Assets/scripts/UI/Exit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+	public void ExitProgram()
+	{
+		Application.Quit();
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#endif
+	}
+}
---
+++ Assets/scripts/UI/MainMenuManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+	[SerializeField]
+	GameObject Main;
+
+	[SerializeField]
+	GameObject GroupManeuvers;
+
+	public void loadTestZone()
+	{
+		SceneManager.LoadScene("TestZone");
+	}
+
+	public void showMain()
+	{
+		Main.SetActive(true);
+		GroupManeuvers.SetActive(false);
+	}
+
+	public void showGroupManeuvers()
+	{
+		Main.SetActive(false);
+		GroupManeuvers.SetActive(true);
+	}
+}
