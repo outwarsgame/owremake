68576e4c7c8606e7de9b5baf76a68f30762d85ed
Mortuis <xf_2020@hotmail.com>
2019-02-12 22:11:36 +0100

    Adding recon mission in Anubis 01. Updating GUI. È

--- Assets/scripts/UI/MainMenuManager.cs
+++ Assets/scripts/UI/MainMenuManager.cs
@@ -9,7 +9,16 @@ public class MainMenuManager : MonoBehaviour
 	GameObject Main;
 
 	[SerializeField]
+	GameObject SoloCampaign;
+
+	[SerializeField]
+	GameObject SingleMission;
+
+	[SerializeField]
 	GameObject GroupManeuvers;
+
+	[SerializeField]
+	GameObject Options;
 
 	public void loadTestZone()
 	{
@@ -19,12 +28,45 @@ public class MainMenuManager : MonoBehaviour
 	public void showMain()
 	{
 		Main.SetActive(true);
+		SoloCampaign.SetActive(false);
+		SingleMission.SetActive(false);
 		GroupManeuvers.SetActive(false);
+		Options.SetActive(false);
+	}
+
+	public void showSoloCampaign()
+	{
+		Main.SetActive(false);
+		SoloCampaign.SetActive(true);
+		SingleMission.SetActive(false);
+		GroupManeuvers.SetActive(false);
+		Options.SetActive(false);
+	}
+
+	public void showSingleMission()
+	{
+		Main.SetActive(false);
+		SoloCampaign.SetActive(false);
+		SingleMission.SetActive(true);
+		GroupManeuvers.SetActive(false);
+		Options.SetActive(false);
 	}
 
 	public void showGroupManeuvers()
 	{
 		Main.SetActive(false);
+		SoloCampaign.SetActive(false);
+		SingleMission.SetActive(false);
 		GroupManeuvers.SetActive(true);
+		Options.SetActive(false);
+	}
+
+	public void showOptions()
+	{
+		Main.SetActive(false);
+		SoloCampaign.SetActive(false);
+		SingleMission.SetActive(false);
+		GroupManeuvers.SetActive(false);
+		Options.SetActive(true);
 	}
 }
